d59431bb30d064e20a40f5ebf647ab5b90da9233
Garrison Shu <garrison.shu@stonybrook.edu>
2021-11-18 13:27:04 -0500

    Prototype almost complete.
    Prototype almost complete. Got to enforce graph rules and implement win state

---
+++ Source/FairKing/FairKingRock.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FairKingRock.h"
+
+// Sets default values
+AFairKingRock::AFairKingRock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFairKingRock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFairKingRock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
